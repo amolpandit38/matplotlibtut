80ad6fd0ff4653da0c6682e87a22eda72a828f41
amolpandit38 <amol38.ap@gmail.com>
2022-08-31 23:17:08 +0530

    changes made to bar chart regarding the use of numpy library to change the position of the string values in the list

--- vid4.py
+++ vid4.py
@@ -3,6 +3,20 @@ import numpy as np
 
 company = ['GOOGLE', 'AMZN', 'MSFT', 'META']
 revenue = [90, 136, 89, 27]
+profit = [40, 2, 34, 12]
 
-plt.bar(company, revenue)
+xPos = np.arange(len(company))
+print(xPos)
+
+plt.xticks(xPos, company)
+
+
+plt.bar(xPos+0.2, revenue, width=0.4, label="Revenue")  # here new array will be created
+plt.bar(xPos-0.2, profit, width=0.4, label="Profits")  # for horizontal chart use "barh"
+plt.xlabel("Companies")
+plt.ylabel("Revenue")
+plt.title("US Tech Stocks")
+
+plt.legend()
+plt.ylabel("Revenue")
 plt.show()
