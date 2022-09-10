d1cc2ba98fb5c2d46dff5deaf2f94d2244536b5c
amolpandit38 <amol38.ap@gmail.com>
2022-09-10 19:45:08 +0530

    How to save chart files to local dir and added generated jpg and pdf

--- vid6.py
+++ vid6.py
@@ -5,4 +5,8 @@ exp_labels = ['Home rent', 'food', 'Phone/internet', 'car', 'other utilities']
 plt.axis('equal')
 plt.pie(exp_vals, labels=exp_labels, radius=1.5, autopct='%0.2f%%',
         explode=[0, 0, 0.2, 0, 0], startangle=45)
+
+plt.savefig("pie_chart.png", bbox_inches='tight', pad_inches=2)
+plt.savefig("pie_chart.pdf", bbox_inches='tight', pad_inches=2)
+# saving chart files to local folder, or you can specify the path
 plt.show()
